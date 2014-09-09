0cff067c6817cbdc52ac6f5565e00b2f0774cb58
Gurrigurr <gnilsson.art@gmail.com>
2014-09-09 15:21:26 +0200

    Visade o trixade för mina lärlingar

--- GroupProjectRestaurangMVC01/Controllers/RestaurantController.cs
+++ GroupProjectRestaurangMVC01/Controllers/RestaurantController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using GroupProjectRestaurangMVC01.Models;
+using GroupProjectRestaurangMVC01.ViewModels;
 
 namespace GroupProjectRestaurangMVC01.Controllers
 {
@@ -11,9 +13,24 @@ namespace GroupProjectRestaurangMVC01.Controllers
         //
         // GET: /Restaurant/
 
-        public ActionResult Index()
+        public ActionResult Index(Guid id)
         {
-            return View();
+            RestaurangViewModel viewModel = new RestaurangViewModel();
+
+            using (RestaurantProjectMVC01Entities db = new RestaurantProjectMVC01Entities())
+            {
+                Restaurant restaurant =
+                    db.Restaurants.Include("ClosedForBookings")
+                        .Include("OpenForBookings")
+                        .Include("Reservations")
+                        .Include("Tables")
+                        .FirstOrDefault(c => c.Id.Equals(id));
+            
+             
+            
+            viewModel.Restaurant = restaurant;
+            }
+            return View(viewModel);
         }
 
     }
---
+++ GroupProjectRestaurangMVC01/ViewModels/RestaurangViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using GroupProjectRestaurangMVC01.Models;
+
+namespace GroupProjectRestaurangMVC01.ViewModels
+{
+    public class RestaurangViewModel
+    {
+        public Restaurant Restaurant { get; set; }
+    }
+}
