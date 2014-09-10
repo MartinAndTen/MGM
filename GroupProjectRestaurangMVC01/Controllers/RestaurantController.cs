bc25fe8153d49567cad753219575b90dcaae93c5
Gurrigurr <gnilsson.art@gmail.com>
2014-09-10 16:11:08 +0200

    Major update till mvc 5 med allt vad det hör till.

--- GroupProjectRestaurangMVC01/Controllers/RestaurantController.cs
+++ GroupProjectRestaurangMVC01/Controllers/RestaurantController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using GroupProjectRestaurangMVC01.Models;
+using GroupProjectRestaurangMVC01.Repository;
 using GroupProjectRestaurangMVC01.ViewModels;
 
 namespace GroupProjectRestaurangMVC01.Controllers
@@ -12,24 +13,12 @@ namespace GroupProjectRestaurangMVC01.Controllers
     {
         //
         // GET: /Restaurant/
+        private RestaurantRepository _restaurantRepository;
 
         public ActionResult Index(Guid id)
         {
             RestaurangViewModel viewModel = new RestaurangViewModel();
-
-            using (RestaurantProjectMVC01Entities db = new RestaurantProjectMVC01Entities())
-            {
-                Restaurant restaurant =
-                    db.Restaurants.Include("ClosedForBookings")
-                        .Include("OpenForBookings")
-                        .Include("Reservations")
-                        .Include("Tables")
-                        .FirstOrDefault(c => c.Id.Equals(id));
-            
-             
-            
-            viewModel.Restaurant = restaurant;
-            }
+            viewModel.Restaurant = _restaurantRepository.GetRestaurantById(id);
             return View(viewModel);
         }
 
--- GroupProjectRestaurangMVC01/Repository/RestaurantRepository.cs
+++ GroupProjectRestaurangMVC01/Repository/RestaurantRepository.cs
@@ -2,10 +2,23 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using GroupProjectRestaurangMVC01.Models;
 
 namespace GroupProjectRestaurangMVC01.Repository
 {
     public class RestaurantRepository
     {
+        public Restaurant GetRestaurantById(Guid id)
+        {
+            using (RestaurantProjectMVC01Entities db = new RestaurantProjectMVC01Entities())
+            {
+                Restaurant result = db.Restaurants.Include("ClosedForBookings")
+                        .Include("OpenForBookings")
+                        .Include("Reservations")
+                        .Include("Tables")
+                        .FirstOrDefault(c => c.Id.Equals(id));
+                return result;
+            }
+        }
     }
 }
