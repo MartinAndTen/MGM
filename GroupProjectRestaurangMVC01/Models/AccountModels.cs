74cf274f34b8c888bbde1fd75760f2e3d48f9d3b
SuperDuperMartin <martinagnalv@gmail.com>
2014-09-12 11:15:54 +0200

    Fixade LostPassword.. TAADAAA

--- GroupProjectRestaurangMVC01/Models/AccountModels.cs
+++ GroupProjectRestaurangMVC01/Models/AccountModels.cs
@@ -74,6 +74,34 @@ namespace GroupProjectRestaurangMVC01.Models
         public bool RememberMe { get; set; }
     }
 
+    public class ResetPasswordModel
+    {
+        [Required]
+        [Display(Name = "User name")]
+        public string UserName { get; set; }
+        [Required]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+
+    }
+
+    public class ResetPasswordConfirmModel
+    {
+
+        public string Token { get; set; }
+
+        [Required]
+        [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
+        [DataType(DataType.Password)]
+        [Display(Name = "New password")]
+        public string NewPassword { get; set; }
+
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirm new password")]
+        [Compare("NewPassword", ErrorMessage = "The new password and confirmation password do not match.")]
+        public string ConfirmPassword { get; set; }
+    }
+
     public class RegisterModel
     {
         [Required]
