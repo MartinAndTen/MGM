55cc64894ed73e8a0d1819fd7464c34068822980
Gurrigurr <gnilsson.art@gmail.com>
2014-09-16 11:45:55 +0200

    fixade image sökvägen

--- GroupProjectRestaurangMVC01/ViewModels/RestaurantViewModel.cs
+++ GroupProjectRestaurangMVC01/ViewModels/RestaurantViewModel.cs
@@ -11,25 +11,38 @@ namespace GroupProjectRestaurangMVC01.ViewModels
     {
         public Restaurant Restaurant { get; set; }
         [Required]
+        [Display(Name="Restaurant Name")]
         public string Name { get; set; }
         [Required]
+        [Display(Name = "Description")]
         public string Description { get; set; }
         [Required]
+        [Display(Name = "Address")]
         public string Address { get; set; }
         [Required]
+        [Display(Name = "Zip-Code")]
         public int Zipcode { get; set; }
         [Required]
+        [Display(Name = "Phone")]
         public string Phone { get; set; }
         [Required]
+        [Display(Name = "City")]
         public string City { get; set; }
+        [Display(Name = "Total seats")]
         public Nullable<int> TotalSeats { get; set; }
+        [Display(Name = "Capacity")]
         public Nullable<int> Capacity { get; set; }
+        [Display(Name = "Maximun Ordersize")]
         public Nullable<int> MaxSeatPerBooking { get; set; }
+        [Display(Name = "Rating")]
         public string Rating { get; set; }
+        [Display(Name = "Photo")]
         public string Photo { get; set; }
         [Required]
+        [Display(Name = "Restaurant email")]
         public string Email { get; set; }
         [Required]
+        [Display(Name = "Open for booking")]
         public bool Activated { get; set; }
         public List<Restaurant> Restaurants { get; set; }
     }
